0988efc6221fcbf8ebec0c84e360328498dbba80
phan thi cam uoc <uocptcgcc210016@fpt.edu.vn>
2023-06-17 00:47:56 +0700

    add source..

---
+++ src/Controller/MainContronllerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainContronllerController extends AbstractController
+{
+    #[Route('/main/contronller', name: 'app_main_contronller')]
+    public function index(): Response
+    {
+        return $this->render('main_contronller/index.html.twig', [
+            'controller_name' => 'MainContronllerController',
+        ]);
+    }
+}
