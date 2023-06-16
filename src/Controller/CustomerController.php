dee39a93c0acc51ce4bbc509b7b151826f48a60a
phan thi cam uoc <uocptcgcc210016@fpt.edu.vn>
2023-06-17 00:47:47 +0700

    add source..

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,8 +6,10 @@ use App\Entity\Customer;
 use App\Entity\Order;
 use App\Repository\CustomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CustomerController extends AbstractController
 {
@@ -29,16 +31,35 @@ public function getallCustomerorderbyascending(CustomerRepository $customerRepos
 }
 
 
-#[Route('/customer/{id}', name: 'app_customer_detail')]
-public function CustomerDetail(CustomerRepository $customerRepository,Customer $customer):Response
+//#[Route('/customer/add/{id}', name: 'app_customer_addd', priority: 1)]
+//public function CustomerDetail(CustomerRepository $customerRepository,Customer $customer):Response
+//{
+//    $Order = $customer->getName()->getValues();
+//    $total = 0;
+//    For($i=0; $i<count($Order);$i++){
+//        $Product = $Order[$i]->getCustomer()->getProduct()->getValues();
+//        for ($j=0; $j<count($Order);$j++) {
+//        }
+//        }
+//            return $this->render('Customer/detail.html.twig',[
+//                'Customer'=>$customer,
+//                'total'=>$total
+//            ]);
+//
+//        }
+#[Route( '/Customer/add',name: 'app_Customer_add',priority: 1)]
+public function addAction (Request $request, CustomerRepository $customerRepository,SluggerInterface$slugger) :Response
 {
-    $Order = $customer->getName()->getValues();
-    $total = 0;
-    For($i=0; $i<count($Order);$i++){
-        $Product = $Order[$i]->get
+    $form=$this->createForm(CustomerType :: Class, new Customer());
+    $form->handleRequest($request);
+
+
+}
     }
-}
-}
+
+
+
+
 
 
 
