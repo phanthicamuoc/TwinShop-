e8f4929ac12c36b7dc6797176d94e89e7b7c1da8
phan thi cam uoc <uocptcgcc210016@fpt.edu.vn>
2023-06-19 19:26:25 +0700

    add source..

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -11,17 +11,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SupplierController extends AbstractController
 {
-  #[Route('/supplier/add', name: 'app_supplier_add')]
-  public function addSupplierAction(Request $request, SupplierRepository $supplierRepository):Response
-  {
-      $supplier = new Supplier();
-      $form = $this->createForm(AddSupplierType::class, $supplier);
-      $form->handleRequest($request);
-      if ($form->isSubmitted()&&$form->isValid()){
-          $supplier = $form->getData();
-          $supplierRepository->save($supplier,true);
-          $supplier
-      }
+    #[Route('/supplier', name: 'app_supplier')]
+    public function index(): Response
+    {
+        return $this->render('supplier/index.html.twig', [
+            'controller_name' => 'SupplierController',
+        ]);
+    }
+    #[Route('/supplier/add', name: 'app_supplier_add')]
+    public function addSupplierAction(Request $request, SupplierRepository $supplierRepository): Response
+    {
+        $supplier = new Supplier();
 
-  }
+        $form = $this->createForm(AddSupplierType::class, $supplier);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $supplier = $form->getData();
+            $supplierRepository->save($supplier, true);
+            $this->addFlash('success', 'Adding supplier successfully!');
+            return $this->redirectToRoute('app_supplier_add');
+        }
+
+        return $this->render('supplier/add.html.twig', [
+            'form' => $form
+        ]);
+    }
 }
